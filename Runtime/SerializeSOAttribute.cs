7b22763e89b9222d748454cebd4999579120a791
Alihan <74368418+Alihan-4108@users.noreply.github.com>
2026-05-29 17:34:47 +0300

    Update SerializeSOAttribute.cs

--- Runtime/SerializeSOAttribute.cs
+++ Runtime/SerializeSOAttribute.cs
@@ -6,11 +6,11 @@ namespace Alihan4108.SerializeScriptableObject
     [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
     public class SerializeSOAttribute : PropertyAttribute
     {
-        public string color;
+        public string color; //Default color is white;
         public string label;
         public TextAnchor titleAlignment;
 
-        public SerializeSOAttribute(string label = "", TextAnchor titleAlignment = TextAnchor.MiddleLeft, string color = "#E05555")
+        public SerializeSOAttribute(string label = "", TextAnchor titleAlignment = TextAnchor.MiddleLeft, string color = "#FFFFFF")
         {
             this.label = label;
             this.titleAlignment = titleAlignment;
